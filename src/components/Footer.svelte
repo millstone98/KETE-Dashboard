ddc0ccdad04d38a2eff52c01b6d91302f8b04d0e
Silvan Mühlestein <silvan12@hotmail.ch>
2024-01-04 22:14:11 +0100

    © added + website title added

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,12 +3,16 @@
 </script>
 
 <style>
-    .top{
+    .pad{
         padding-top: 30px;
         padding-bottom: 50px;
+        text-align: center;
+        margin: auto;
+        
     }
 </style>
 
-<div class="top">
+<div class="pad">
+    <p>© Antenen, Bernhardsgrütter, Galliker, Mühlestein, Pauger</p>
     <img src={ostlogo} alt="OST Logo" style="display: block; margin: 0 auto; vertical-align: middle;">
 </div>
